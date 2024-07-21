34a466230e25069ad1615f289ffe28ce3fce2e44
Nicholas Yang <nick@nicholasyang.com>
2024-07-20 23:07:35 -0400

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -29,15 +29,16 @@ const pullRequestsQuery = gql`
 
 export default async function Home() {
   const session = await auth();
-  const { data } = await query({ query: pullRequestsQuery });
-  const pullRequests = data.viewer.pullRequests.nodes.map(
-    (node: GitHubPullRequest) => ({
-      title: node.title,
-      number: node.number,
-      status: node.state === "MERGED" ? Status.Merged : Status.Ready,
-      repository: `${node.repository.owner.login}/${node.repository.name}`,
-    }),
-  );
+  // const { data } = await query({ query: pullRequestsQuery });
+  // const pullRequests = data.viewer.pullRequests.nodes.map(
+  //   (node: GitHubPullRequest) => ({
+  //     title: node.title,
+  //     number: node.number,
+  //     status: node.state === "MERGED" ? Status.Merged : Status.Ready,
+  //     repository: `${node.repository.owner.login}/${node.repository.name}`,
+  //   }),
+  // );
+  const pullRequests: PullRequest[] = [];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
